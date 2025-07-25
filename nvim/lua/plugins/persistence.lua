6f8df038dc034b9db7c2c6a59229ef84edb8b759
Ishmum Jawad Khan <ishmum@kahf.co>
2025-07-25 14:16:52 +0600

    Add persistent sessions

---
+++ nvim/lua/plugins/persistence.lua
@@ -0,0 +1,36 @@
+return {
+  "folke/persistence.nvim",
+  lazy = false, -- ensures plugin is loaded at startup
+  opts = {
+    dir = vim.fn.stdpath("state") .. "/sessions/",
+    need = 1, -- only save if at least one buffer is open
+    branch = true, -- separate sessions per Git branch
+  },
+  config = function(_, opts)
+    require("persistence").setup(opts)
+
+    -- Auto-restore session on startup, only if no file specified
+    vim.api.nvim_create_autocmd("VimEnter", {
+      nested = true,
+      callback = function()
+        if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+          require("persistence").load()
+        end
+      end,
+    })
+
+    -- Optional keymaps
+    vim.keymap.set("n", "<leader>qs", function()
+      require("persistence").load()
+    end, { desc = "Restore session for current directory" })
+    vim.keymap.set("n", "<leader>qS", function()
+      require("persistence").select()
+    end, { desc = "Select a session to restore" })
+    vim.keymap.set("n", "<leader>ql", function()
+      require("persistence").load({ last = true })
+    end, { desc = "Restore last session" })
+    vim.keymap.set("n", "<leader>qd", function()
+      require("persistence").stop()
+    end, { desc = "Disable auto-saving this session" })
+  end,
+}
